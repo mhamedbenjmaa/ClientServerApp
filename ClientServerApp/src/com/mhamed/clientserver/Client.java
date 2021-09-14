f0900d785261e0087b63de652424da401d0218e9
0B6136649 <0B6136649@9.211.127.115>
2021-09-14 13:30:31 -0400

    commit first source

---
+++ ClientServerApp/src/com/mhamed/clientserver/Client.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mhamed.clientserver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 0B6136649
+ */
+public class Client {
+
+    private Socket socket;
+    private Scanner scanner;
+
+    private Client(InetAddress serverAddress, int serverPort) throws Exception {
+        this.socket = new Socket(serverAddress, serverPort);
+        this.scanner = new Scanner(System.in);
+    }
+
+    private void start() throws IOException {
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+            out.println(input);
+            out.flush();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Client client = new Client(
+                InetAddress.getByName("9.211.127.115"),
+                Integer.parseInt("58140"));
+
+        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+        client.start();
+    }
+
+}
