7d3c765ea209a2b88ef931bbfe8a0facd77be5ac
0B6136649 <0B6136649@9.211.127.115>
2022-03-07 11:37:30 -0500

    new client app

---
+++ ClientApp/src/clientapp/ClientApp.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clientapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 0B6136649
+ */
+public class ClientApp {
+
+    private final Socket socket;
+    private final Scanner scanner;
+
+    private ClientApp(InetAddress serverAddress, int serverPort) throws Exception {
+        this.socket = new Socket(serverAddress, serverPort);
+        this.scanner = new Scanner(System.in);
+    }
+
+    private void start() throws IOException {
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+            out.println(input);
+            out.flush();
+        }
+    }
+
+    public static String getAddress() {
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            //System.out.println("Your current IP address : " + ip);
+            //System.out.println("Your current Hostname : " + hostname);
+            return ("Your current IP address : " + ip + "\n" + "Your current Hostname : " + hostname);
+
+        } catch (UnknownHostException e) {
+
+            return (e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(getAddress());
+        System.out.println("try to connect to " + args[0] + ":" + args[1]);
+        ClientApp client = new ClientApp(
+                InetAddress.getByName(args[0]),
+                Integer.parseInt(args[1]));
+
+        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+        client.start();
+    }
+
+}
